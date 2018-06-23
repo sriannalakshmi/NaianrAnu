aa1daa1620e369187af55593a52b928ae3e17b13
sriannalakshmi <40511622+sriannalakshmi@users.noreply.github.com>
2018-06-23 05:54:37 +0000

    Create Pgm7.java

---
+++ Pgm7.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Pgm7
+{ 
+ public static void main(String args[])
+ {
+  Scanner s = new Scanner(System.in);
+  int number = s.nextInt();
+  for(int i=1;i<=number;i++)
+  {
+      System.out.print("Hello");
+      System.out.println();
+  }
+ }
+}
