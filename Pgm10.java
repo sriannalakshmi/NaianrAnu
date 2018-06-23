8c87b01024e249d6e7d970f09a2054d28ccb264d
sriannalakshmi <40511622+sriannalakshmi@users.noreply.github.com>
2018-06-23 06:18:05 +0000

    Create Pgm10.java

---
+++ Pgm10.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Pgm10
+{ 
+ public static void main(String args[])
+ {
+  Scanner s = new Scanner(System.in);
+  int number = s.nextInt();
+  String s0 = String.valueOf(number);
+  System.out.print(s0.length());
+   }
+}
