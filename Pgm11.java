a3d5ac83a3169944f0d88a06cc44b38651ba08c8
sriannalakshmi <40511622+sriannalakshmi@users.noreply.github.com>
2018-06-23 06:22:22 +0000

    Create Pgm11.java

---
+++ Pgm11.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class Pgm11
+{ 
+ public static void main(String args[])
+ {
+  Scanner s = new Scanner(System.in);
+  int number = s.nextInt();
+  int number1 = s.nextInt();
+  System.out.print((int)Math.pow(number,number1));
+   }
+}
